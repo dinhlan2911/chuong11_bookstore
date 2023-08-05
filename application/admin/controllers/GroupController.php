eb1d7cae5f74d65a41ec0c45f294b37b47f2c08f
dinhlan2911 <dinhlan.com@gmail.com>
2023-08-05 23:12:08 +0700

    updated

--- application/admin/controllers/GroupController.php
+++ application/admin/controllers/GroupController.php
@@ -3,11 +3,11 @@ class GroupController extends Controller
 {
 	public function __construct($arrParams)
 	{
-		// parent::__construct($arrParams);
-		// $this->_templateObj->setFolderTemplate('student/main/');
-		// $this->_templateObj->setFileTemplate('index.php');
-		// $this->_templateObj->setFileConfig('template.ini');
-		// $this->_templateObj->load();
+		parent::__construct($arrParams);
+		$this->_templateObj->setFolderTemplate('student/main/');
+		$this->_templateObj->setFileTemplate('index.php');
+		$this->_templateObj->setFileConfig('template.ini');
+		$this->_templateObj->load();
 	}
 
 	// Hiển thị danh sách group
