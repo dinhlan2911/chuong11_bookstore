5a2d9c5f9023ab84571cb691d2954d8ab9207770
dinhlan2911 <dinhlan.com@gmail.com>
2023-08-06 21:12:24 +0700

    update

--- application/admin/controllers/GroupController.php
+++ application/admin/controllers/GroupController.php
@@ -4,7 +4,7 @@ class GroupController extends Controller
 	public function __construct($arrParams)
 	{
 		parent::__construct($arrParams);
-		$this->_templateObj->setFolderTemplate('student/main/');
+		$this->_templateObj->setFolderTemplate('admin/main/');
 		$this->_templateObj->setFileTemplate('index.php');
 		$this->_templateObj->setFileConfig('template.ini');
 		$this->_templateObj->load();
